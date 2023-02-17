4c877997f3427302b4bfa9a625bcd9a0ccc7cecf
Lalasince <125644386+Lalasince@users.noreply.github.com>
2023-02-17 15:52:05 +0700

    Update HW-w2.c

--- C/HW-w2.c
+++ C/HW-w2.c
@@ -48,16 +48,16 @@
 
  //5.
 
- #include<stdio.h>
- int main ()
- {
-    int inp=20599;
-    int y=(inp/365);
-    int m=(inp%365)/30;
-    int d=(inp%365)-(m*30);
-    printf("Jack Ma has been living in this world for %d years, %d months,and %d days.",y,m,d);
-    return(0);
- }
+//  #include<stdio.h>
+//  int main ()
+//  {
+//     int inp=20599;
+//     int y=(inp/365);
+//     int m=(inp%365)/30;
+//     int d=(inp%365)-(m*30);
+//     printf("Jack Ma has been living in this world for %d years, %d months,and %d days.",y,m,d);
+//     return(0);
+//  }
 
 
  //6.
