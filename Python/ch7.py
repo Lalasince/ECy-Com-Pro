bd5bbdb5a48eeec321aac248a23e32e9d1e89b6c
Lalasince <125644386+Lalasince@users.noreply.github.com>
2023-05-10 09:49:44 +0700

    Update ch7.py

--- Python/ch7.py
+++ Python/ch7.py
@@ -23,34 +23,47 @@
 
 
 
-
+# CHAT GPT
 # Define the function f(x) and its derivative f'(x)
-def f(x):
-    return x**2 + 6*x + 5
+# def f(x):
+#     return x**2 + 6*x + 5
 
-def f_prime(x):
-    return 2*x + 6
+# def f_prime(x):
+#     return 2*x + 6
 
-# Set the initial guess for the root
-x0 = 1.0
+# # Set the initial guess for the root
+# x0 = 1.0
 
-# Set a tolerance for the error and a maximum number of iterations
-tolerance = 1e-6
-max_iterations = 100
+# # Set a tolerance for the error and a maximum number of iterations
+# tolerance = 1e-6
+# max_iterations = 100
 
-# Initialize the iteration counter and the error
-iteration = 0
-error = abs(f(x0) / f_prime(x0))
+# # Initialize the iteration counter and the error
+# iteration = 0
+# error = abs(f(x0) / f_prime(x0))
 
-# Start the loop
-while error > tolerance and iteration < max_iterations:
-    x1 = x0 - f(x0) / f_prime(x0)  # Compute the next approximation of the root
-    error = abs((x1 - x0) / x1)    # Compute the relative error
-    x0 = x1                        # Update the approximation of the root
-    iteration += 1                 # Increment the iteration counter
+# # Start the loop
+# while error > tolerance and iteration < max_iterations:
+#     x1 = x0 - f(x0) / f_prime(x0)  # Compute the next approximation of the root
+#     error = abs((x1 - x0) / x1)    # Compute the relative error
+#     x0 = x1                        # Update the approximation of the root
+#     iteration += 1                 # Increment the iteration counter
 
 # Print the result
-if iteration == max_iterations:
-    print("Failed to converge to a root within the maximum number of iterations.")
-else:
-    print("The root is approximately:", x0)
+# if iteration == max_iterations:
+#     print("Failed to converge to a root within the maximum number of iterations.")
+# else:
+#     print("The root is approximately:", x0)
+
+
+a = float(input(">>"))
+x0 = a
+
+error = 1e-10
+f= abs((x0**2+6*x0+5)/(2*x0+6))
+
+while f > error:
+    x1 = x0 - ((x0**2+6*x0+5)/(2*x0+6))
+    f = abs((x1-x0)/x1)
+    x0=x1
+print( "sqrt(",a,") =", x0 )
